1425c99d81085b5ab29e6f2674aa16f5d7680f1f
oboki <oboki@kakao.com>
2024-07-25 01:35:42 +0900

    test: simple json server

---
+++ tests/simple_http_server.py
@@ -0,0 +1,24 @@
+import os
+import json
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+
+
+class JSONDirectoryHandler(SimpleHTTPRequestHandler):
+    def list_directory(self, path):
+        try:
+            encoded = json.dumps(os.listdir(path)).encode("utf-8")
+            self.send_response(200)
+            self.send_header("Content-type", "application/json; charset=utf-8")
+            self.send_header("Content-Length", str(len(encoded)))
+            self.end_headers()
+            self.wfile.write(encoded)
+        except os.error:
+            self.send_error(404, "No permission to list directory")
+
+
+if __name__ == "__main__":
+    port = 8000
+    server_address = ("", port)
+    httpd = HTTPServer(server_address, JSONDirectoryHandler)
+    print(f"Serving on port {port}")
+    httpd.serve_forever()
