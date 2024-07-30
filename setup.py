d4b621fd4cd4308c24505b42dee37d6f9e7f0ea4
oboki <oboki@kakao.com>
2024-07-31 01:02:10 +0900

    chore: update version to 0.1.5

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="metatreedb",
-    version="0.1.4",
+    version="0.1.5",
     author="oboki",
     author_email="oboki@kakao.com",
-    description="File system-based, easy-to-read database",
+    description="Metatree is a DBMS that uses the filesystem itself as a tree-structured database.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/oboki/metatreedb",
     packages=find_packages(),
-    install_requires=["pyyaml", "requests"],
+    install_requires=["requests"],
     extras_require={"dev": ["pytest"]},
     classifiers=[
         "Programming Language :: Python :: 3",
