74a1caf7b1c9300ba091f71d4a708a8d3ddfac55
oboki <oboki@kakao.com>
2024-11-21 00:41:05 +0900

    chore: update version to 0.2.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="metatreedb",
-    version="0.2.2",
+    version="0.2.3",
     author="oboki",
     author_email="oboki@kakao.com",
     description="Metatree is a DBMS that uses the filesystem itself as a tree-structured database.",
